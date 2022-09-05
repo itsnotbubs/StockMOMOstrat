8fef89eac60ed434925268b055a15b1d8f9af618
itsnotbubs <100372323+itsnotbubs@users.noreply.github.com>
2022-09-05 05:45:39 -0400

    main

---
+++ MOMO.py
@@ -0,0 +1,169 @@
+import numpy as np #The Numpy numerical computing library
+import pandas as pd #The Pandas data science library
+import requests #The requests library for HTTP requests in Python
+import xlsxwriter #The XlsxWriter libarary for 
+import math #The Python math module
+from scipy import stats #The SciPy stats module
+
+
+df = pd.read_csv('Stocks in the SP 500 Index.csv')
+
+
+
+from sec import IEX_CLOUD_API_TOKEN
+
+# Function sourced from 
+# https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]   
+        
+symbol_groups = list(chunks(df['Symbol'], 100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+#     print(symbol_strings[i])
+
+
+hqm_columns = [
+                'Ticker', 
+                'Price', 
+                'Number of Shares to Buy', 
+                'One-Year Price Return', 
+                'One-Year Return Percentile',
+                'Six-Month Price Return',
+                'Six-Month Return Percentile',
+                'Three-Month Price Return',
+                'Three-Month Return Percentile',
+                'One-Month Price Return',
+                'One-Month Return Percentile',
+                'HQM Score'
+                ]
+
+hqm_dataframe = pd.DataFrame(columns = hqm_columns)
+
+for symbol_string in symbol_strings:
+#     print(symbol_strings)
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=stats,quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url).json()
+    for symbol in symbol_string.split(','):
+        hqm_dataframe = hqm_dataframe.append(
+                                        pd.Series([symbol, 
+                                                   data[symbol]['quote']['latestPrice'],
+                                                   'N/A',
+                                                   data[symbol]['stats']['year1ChangePercent'],
+                                                   'N/A',
+                                                   data[symbol]['stats']['month6ChangePercent'],
+                                                   'N/A',
+                                                   data[symbol]['stats']['month3ChangePercent'],
+                                                   'N/A',
+                                                   data[symbol]['stats']['month1ChangePercent'],
+                                                   'N/A',
+                                                   'N/A'
+                                                   ], 
+                                                  index = hqm_columns), 
+                                        ignore_index = True)
+        
+# hqm_dataframe.columns
+
+time_periods = [
+                'One-Year',
+                'Six-Month',
+                'Three-Month',
+                'One-Month'
+                ]
+
+for row in hqm_dataframe.index:
+    for time_period in time_periods:
+        hqm_dataframe.loc[row, f'{time_period} Return Percentile'] = stats.percentileofscore(hqm_dataframe[f'{time_period} Price Return'], hqm_dataframe.loc[row, f'{time_period} Price Return'])/100
+
+# Print each percentile score to make sure it was calculated properly
+for time_period in time_periods:
+    print(hqm_dataframe[f'{time_period} Return Percentile'])
+
+#Print the entire DataFrame    
+hqm_dataframe
+
+
+from statistics import mean
+
+for row in hqm_dataframe.index:
+    momentum_percentiles = []
+    for time_period in time_periods:
+        momentum_percentiles.append(hqm_dataframe.loc[row, f'{time_period} Return Percentile'])
+    hqm_dataframe.loc[row, 'HQM Score'] = mean(momentum_percentiles)
+
+
+hqm_dataframe.sort_values(by = 'HQM Score', ascending = False)
+hqm_dataframe = hqm_dataframe[:51]
+
+portfolio_input()
+
+position_size = float(portfolio_size) / len(hqm_dataframe.index)
+for i in range(0, len(hqm_dataframe['Ticker'])-1):
+    hqm_dataframe.loc[i, 'Number of Shares to Buy'] = math.floor(position_size / hqm_dataframe['Price'][i])
+hqm_dataframe
+
+
+writer = pd.ExcelWriter('momentum_strategy.xlsx', engine='xlsxwriter')
+hqm_dataframe.to_excel(writer, sheet_name='Momentum Strategy', index = False)
+
+
+background_color = '#0a0a23'
+font_color = '#ffffff'
+
+string_template = writer.book.add_format(
+        {
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+dollar_template = writer.book.add_format(
+        {
+            'num_format':'$0.00',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+integer_template = writer.book.add_format(
+        {
+            'num_format':'0',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+percent_template = writer.book.add_format(
+        {
+            'num_format':'0.0%',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+
+column_formats = { 
+                    'A': ['Ticker', string_template],
+                    'B': ['Price', dollar_template],
+                    'C': ['Number of Shares to Buy', integer_template],
+                    'D': ['One-Year Price Return', percent_template],
+                    'E': ['One-Year Return Percentile', percent_template],
+                    'F': ['Six-Month Price Return', percent_template],
+                    'G': ['Six-Month Return Percentile', percent_template],
+                    'H': ['Three-Month Price Return', percent_template],
+                    'I': ['Three-Month Return Percentile', percent_template],
+                    'J': ['One-Month Price Return', percent_template],
+                    'K': ['One-Month Return Percentile', percent_template],
+                    'L': ['HQM Score', integer_template]
+                    }
+
+for column in column_formats.keys():
+    writer.sheets['Momentum Strategy'].set_column(f'{column}:{column}', 20, column_formats[column][1])
+    writer.sheets['Momentum Strategy'].write(f'{column}1', column_formats[column][0], string_template)
